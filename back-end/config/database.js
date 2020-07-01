dd00f1022c7f9711f39a1fe2693dcf8096ffffff
V Rohan Rao <rohanrao.dec11@gmail.com>
2020-07-01 21:26:59 +0530

    removed

--- back-end/config/database.js
+++
@@ -1,9 +0,0 @@
-const server = 'testcluster-xrkro.mongodb.net'
-const database = 'test?retryWrites=true&w=majority'
-const user = 'calRohan'
-const password = '1234'
-
-module.exports = {
-    'db' : `mongodb+srv://${user}:${password}@${server}/${database}`,
-    'secret' : 'rohan'
-}
