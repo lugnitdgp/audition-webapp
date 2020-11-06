0d437cab17eaff891cd39f339704c5a51265bf9c
rohan <rohan@pop-os.localdomain>
2020-11-06 16:50:22 +0530

    React frontend

---
+++ front-end/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import MenuAppBar from "../components/Header/Header"
+
+import SwipeableTemporaryDrawer from "../components/Drawer"
+
+export default function Home() {
+  return(
+    <>
+    <MenuAppBar/>
+    <SwipeableTemporaryDrawer/>
+    </>
+  )
+}
