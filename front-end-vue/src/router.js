f0d5e1a23100655a181a1e5aac1a0b3a5a822f2b
error-200 <yashv8696@gmail.com>
2020-12-04 12:09:54 +0530

    [+] Addround Basic Layout

--- front-end-vue/src/router.js
+++ front-end-vue/src/router.js
@@ -67,6 +67,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/UserControl.vue')
+    },
+    {
+      path: '/Addround',
+      name: 'Addround',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Addround.vue')
     }
     
   ]
---
+++ front-end-vue/src/views/Addround.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container>
+        <v-text-field
+           v-model="newTodo"
+          label="What are you working on?"
+          solo
+          @keydown.enter="addTodo"
+        >
+        
+        </v-text-field>
+        <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+       
+        
+
+        <v-card class="mx-auto"
+              color="#26c6da"
+              dark
+              min-width="600" 
+              min-height="100" 
+              v-for="(todo, index) in todos" :key="todo.id">
+            <v-card-title>
+          <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon >mdi-pen</v-icon>
+              </v-btn>
+
+              <v-btn icon   @click="removeTodo(index)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+
+            </v-card-actions>
+     
+      
+    </v-card-title>
+    
+ <v-card-text>
+            <div>{{ todo.text }}</div>
+             </v-card-text>
+         </v-card>
+        
+</draggable>
+  
+      </v-container>
+    </v-content>
+  </v-app>
+
+
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+   components: {
+            draggable,
+        },
+  data: () => ({
+    newTodo: '',
+    todos: [],
+  }),
+  methods: {
+    addTodo() {
+      this.todos.push({ text: this.newTodo, completed: false });
+      this.newTodo = '';
+    },
+     removeTodo(index) {
+      this.todos.splice(index,1);
+  
+    },
+  },
+  mounted() {
+    console.log('App mounted!');
+    if (localStorage.getItem('todos')) this.todos = JSON.parse(localStorage.getItem('todos'));
+    else localStorage.removeItem('todos');
+  },
+  watch: {
+    todos: {
+      handler() {
+        console.log('Todos changed!');
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+      },
+      deep: true,
+    },
+  },
+}
+</script>
+<style scoped>
+
+</style>
